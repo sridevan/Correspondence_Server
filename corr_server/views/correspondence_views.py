a7cb0d51fb49752d18926cfffba0b41e7bf04dc3
Sri Devan <sria@bgsu.edu>
2020-04-13 18:30:13 -0400

    Removed duplicate code

--- corr_server/views/correspondence_views.py
+++ corr_server/views/correspondence_views.py
@@ -119,7 +119,7 @@ def correspondence_geometric(method, ife, selection, core):
         query_type = pi.check_query(query_list)
         query_units = qs.get_query_units(query_type, query_list, query_ife)
         rejected_members, ec_members, ec_id, nr_release = em.get_ec_members(query_ife)
-        corr_complete, corr_std = cs.get_correspondence(query_units, ec_members)
+        corr_complete, corr_std = cs.get_correspondence(query_units, ec_members[:10])
         ife_list, coord_data = ui.build_coord(corr_complete)
         # Get the pairwise annotation for the instances in the EC
         pw_info, pw_sorted = ps.get_pairwise_annotation(corr_complete, query_units, ife_list)
@@ -161,8 +161,6 @@ def correspondence_geometric(method, ife, selection, core):
         rejected_members, ec_members, ec_id, nr_release = em.get_ec_members(query_ife)
         # Get correspondence for the query nts
         corr_complete, corr_std = cs.get_correspondence(query_units, ec_members)
-        # Get correspondence for the query nts
-        corr_complete, corr_std = cs.get_correspondence(query_units, ec_members)
         # Get correspondence for the core nts
         core_complete = cs.get_correspondence_core(core_units, ec_members)
         ife_list, coord_data = ui.build_coord_relative(core_complete, corr_complete)
@@ -178,11 +176,6 @@ def correspondence_geometric(method, ife, selection, core):
         ifes_ordered, coord_ordered = ui.get_ordering(ife_list, discrepancy_data, coord_data)
         # Get discrepancy statistics and build the heatmap data for display
         max_disc, percentile, heatmap_data = ui.build_heatmap_data(discrepancy_data, ifes_ordered)
-        '''
-        # Get all the annotation from the definition file
-        trna_occupancy, functional_state, factors_bound, antibiotic_bound, reported_intersubunit, \
-        calculated_intersubunit, reported_head, calculated_head = ui.get_annotation(ifes_ordered)
-        '''
         # Get all the annotation from the definition file
         trna_occupancy, functional_state, factors_bound, antibiotic_bound, reported_intersubunit, \
         calculated_intersubunit, reported_head, calculated_head, description, \
@@ -190,15 +183,6 @@ def correspondence_geometric(method, ife, selection, core):
         publication_year = ui.get_annotation_new(ifes_ordered)
         # Reorder the pairwise annotation based on the new ordering
         pw_info_ordered = ui.reorder_pw(ifes_ordered, pw_info)
-        '''
-        return render_template("correspondence_display.html", query_nts=query_units,
-                               coord=coord_ordered, ifes=ifes_ordered, maxDisc=max_disc,
-                               p2=percentile, data=heatmap_data, trna_occupancy=trna_occupancy,
-                               functional_state=functional_state, factors_bound=factors_bound,
-                               reported_rotation=reported_intersubunit, calculated_rotation=calculated_intersubunit,
-                               reported_head=reported_head, calculated_head=calculated_head,
-                               antibiotic_bound=antibiotic_bound, pw_info=pw_info_ordered, pw_list=pw_sorted)
-        '''
         return render_template("correspondence_display.html", query_nts=query_units,
                                coord=coord_ordered, coord_core=None, ifes=ifes_ordered, maxDisc=max_disc, p2=percentile,
                                data=heatmap_data, trna_occupancy=trna_occupancy, functional_state=functional_state,
