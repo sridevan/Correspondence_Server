abb3e8d31e0db067660f4e3200f763fa8d1b0eac
Sri Devan <sria@bgsu.edu>
2020-06-03 19:06:59 -0400

    Removed unused code

--- corr_server/views/correspondence_views.py
+++ corr_server/views/correspondence_views.py
@@ -12,31 +12,9 @@ import infrastructure.utility as ui
 
 blueprint = flask.Blueprint('correspondence', __name__, template_folder='templates')
 
-ife_trna = [('4V50', 'AA'), ('4V50', 'CA'), ('4V9D', 'AA'), ('4V9D', 'BA'), ('5MDZ', '2'), ('6BU8', 'A'), ('6GWT', 'a'),
-            ('6GXM', 'a'), ('6GXN', 'a'), ('6GXO', 'a'), ('4V9C', 'AA'), ('4V9C', 'CA'), ('4WOI', 'AA'), ('4WOI', 'DA'),
-            ('5KCR', '1a'), ('5KCS', '1a'), ('3J9Y', 'a'), ('3JCD', 'a'), ('3JCE', 'a'), ('5UYK', 'A'),
-            ('5UYL', 'A'), ('5UYM', 'A'), ('5UYN', 'A'), ('5UYP', 'A'), ('5UYQ', 'A'), ('5WDT', 'a'), ('5WE4', 'a'),
-            ('5WE6', 'a'), ('5WF0', 'a'), ('5WFS', 'a'), ('3J9Z', 'SA'), ('3JA1', 'SA'), ('3JCJ', 'g'),
-            ('6DNC', 'A'), ('5NP6', 'D'), ('6H4N', 'a'), ('5H5U', 'h'), ('5MDV', '2'), ('5MDW', '2'), ('5MDY', '2'),
-            ('5MGP', 'a'), ('5U4I', 'a'), ('5U4J', 'a'), ('5U9F', 'A'), ('5U9G', 'A'), ('6ENF', 'a'), ('6ENJ', 'a'),
-            ('6ENU', 'a'), ('6C4I', 'a'), ('3JBU', 'A'), ('3JBV', 'A'), ('5JTE', 'AA'), ('5JU8', 'AA'), ('5NWY', '0'),
-            ('5O2R', 'a'), ('5LZD', 'a'), ('5IQR', '2'), ('5KPS', '27'), ('5KPW', '26'), ('5KPX', '26'),
-            ('5L3P', 'a'), ('4V6D', 'AA'), ('4V6D', 'CA'), ('4V6E', 'AA'), ('4V6E', 'CA'), ('4V55', 'AA'),
-            ('4V55', 'CA'), ('4V9P', 'BA'), ('4V9P', 'DA'), ('4V9P', 'FA'), ('4V9P', 'HA'),
-            ('4V9O', 'BA'), ('4V9O', 'DA'), ('4V9O', 'FA'), ('4V9O', 'HA'), ('4V85', 'AA'), ('4V89', 'AA'),
-            ('4V54', 'AA'), ('4V54', 'CA')]
+bound = [('4V9D', 'AA'), ('4V9D', 'BA'), ('4V9O', 'BA'), ('4V9O', 'DA'), ('4V9O', 'FA'), ('4V9O', 'HA'), ('4V9P', 'BA'), ('4V9P', 'DA'), ('4V9P', 'FA'), ('4V9P', 'HA'), ('6GWT', 'a'), ('6GXM', 'a'), ('6GXN', 'a'), ('6GXO', 'a'), ('4V54', 'AA'), ('4V54', 'CA'), ('4V55', 'AA'), ('4V55', 'CA'), ('4V9C', 'CA'), ('5KCS', '1a'), ('3J9Y', 'a'), ('3JCE', 'a'), ('5AFI', 'a'), ('5UYK', 'A'), ('5UYL', 'A'), ('5UYM', 'A'), ('5UYN', 'A'), ('5UYP', 'A'), ('5UYQ', 'A'), ('5WDT', 'a'), ('5WE4', 'a'), ('5WE6', 'a'), ('5WF0', 'a'), ('5WFK', 'a'), ('5WFS', 'a'), ('3J9Z', 'SA'), ('3JA1', 'SA'), ('3JCJ', 'g'), ('6DNC', 'A'), ('5NP6', 'D'), ('6H4N', 'a'), ('5H5U', 'h'), ('5MDV', '2'), ('5MDW', '2'), ('5MDY', '2'), ('5MGP', 'a'), ('5U4I', 'a'), ('5U9F', 'A'), ('5U9G', 'A'), ('6ENF', 'a'), ('6ENJ', 'a'), ('6ENU', 'a'), ('6C4I', 'a'), ('3JBU', 'A'), ('3JBV', 'A'), ('5JTE', 'AA'), ('5JU8', 'AA'), ('5NWY', '0'), ('5O2R', 'a'), ('5LZD', 'a'), ('5IQR', '2'), ('5KPS', '27'), ('5KPW', '26'), ('5KPX', '26'), ('5L3P', 'a'), ('4V85', 'AA'), ('4V89', 'AA'), ('3JCD', 'a'), ('6O9J', 'a'), ('6O9K', 'a'), ('6BY1', 'BA'), ('6BY1', 'AA'), ('6ORE', '2'), ('6OSQ', '2'), ('6ORL', '2'), ('6OUO', '2'), ('6OT3', '2'), ('6OSK', '2'), ('6Q97', '2'), ('6Q9A', '2'), ('6NQB', 'A')]
 
-ife_empty = [('4V4Q', 'AA'), ('4V4Q', 'CA'), ('4V5B', 'BA'), ('4V5B', 'DA'),
-             ('4YBB', 'AA'), ('4YBB', 'BA'), ('5IT8', 'AA'), ('5IT8', 'BA'), ('5J5B', 'AA'), ('5J5B', 'BA'),
-             ('5J7L', 'AA'), ('5J7L', 'BA'), ('5J88', 'AA'), ('5J88', 'BA'), ('5J8A', 'AA'), ('5J8A', 'BA'),
-             ('5J91', 'AA'), ('5J91', 'BA'), ('5JC9', 'AA'), ('5JC9', 'BA'), ('6I7V', 'AA'), ('6I7V', 'BA'),
-             ('4U1U', 'AA'), ('4U1U', 'CA'), ('4U1V', 'AA'), ('4U1V', 'CA'), ('4U20', 'AA'), ('4U20', 'CA'),
-             ('4U24', 'AA'), ('4U24', 'CA'), ('4U25', 'AA'), ('4U25', 'CA'), ('4U26', 'AA'), ('4U26', 'CA'),
-             ('4U27', 'AA'), ('4U27', 'CA'), ('4V4H', 'AA'), ('4V4H', 'CA'), ('4V52', 'AA'), ('4V52', 'CA'),
-             ('4V53', 'AA'), ('4V53', 'CA'), ('4V56', 'AA'), ('4V56', 'CA'), ('4V57', 'AA'), ('4V57', 'CA'),
-             ('4V64', 'AA'), ('4V64', 'CA'), ('4V7S', 'AA'), ('4V7S', 'CA'), ('4V7T', 'AA'), ('4V7T', 'CA'),
-             ('4V7U', 'AA'), ('4V7U', 'CA'), ('4V7V', 'AA'), ('4V7V', 'CA'), ('4WF1', 'AA'), ('4WF1', 'CA'),
-             ('4WWW', 'QA'), ('4WWW', 'XA'), ('4V6C', 'AA'), ('4V6C', 'CA')]
+empty = [('4V4Q', 'AA'), ('4V4Q', 'CA'), ('4V50', 'AA'), ('4V50', 'CA'), ('4V5B', 'BA'), ('4V5B', 'DA'), ('4YBB', 'AA'), ('4YBB', 'BA'), ('5IT8', 'AA'), ('5IT8', 'BA'), ('5J5B', 'AA'), ('5J5B', 'BA'), ('5J7L', 'AA'), ('5J7L', 'BA'), ('5J88', 'AA'), ('5J88', 'BA'), ('5J8A', 'AA'), ('5J8A', 'BA'), ('5J91', 'AA'), ('5J91', 'BA'), ('5JC9', 'AA'), ('5JC9', 'BA'), ('5MDZ', '2'), ('6BU8', 'A'), ('4U1U', 'AA'), ('4U1U', 'CA'), ('4U1V', 'AA'), ('4U1V', 'CA'), ('4U20', 'AA'), ('4U20', 'CA'), ('4U24', 'AA'), ('4U24', 'CA'), ('4U25', 'AA'), ('4U25', 'CA'), ('4U26', 'AA'), ('4U26', 'CA'), ('4U27', 'AA'), ('4U27', 'CA'), ('4V4H', 'AA'), ('4V4H', 'CA'), ('4V52', 'AA'), ('4V52', 'CA'), ('4V53', 'AA'), ('4V53', 'CA'), ('4V56', 'AA'), ('4V56', 'CA'), ('4V57', 'AA'), ('4V57', 'CA'), ('4V64', 'AA'), ('4V64', 'CA'), ('4V7S', 'AA'), ('4V7S', 'CA'), ('4V7T', 'AA'), ('4V7T', 'CA'), ('4V7U', 'AA'), ('4V7U', 'CA'), ('4V7V', 'AA'), ('4V7V', 'CA'), ('4V9C', 'AA'), ('4WF1', 'AA'), ('4WF1', 'CA'), ('4WOI', 'AA'), ('4WOI', 'DA'), ('4WWW', 'QA'), ('4WWW', 'XA'), ('5KCR', '1a'), ('5LZA', 'a'), ('4V6C', 'AA'), ('4V6C', 'CA'), ('4V6D', 'AA'), ('4V6D', 'CA'), ('4V6E', 'AA'), ('4V6E', 'CA')]
 
 default_ordering = [('0', '5AFI|1|a'), ('1', '5UYM|1|A'), ('2', '5LZD|1|a'), ('3', '5WDT|1|a'), ('4', '5WE4|1|a'),
                     ('5', '5WE6|1|a'), ('6', '3JCE|1|a'), ('7', '5WFS|1|a'), ('8', '6ENJ|1|a'), ('9', '6BU8|1|A'),
@@ -119,7 +97,7 @@ def correspondence_geometric(method, ife, selection, core):
         query_type = pi.check_query(query_list)
         query_units = qs.get_query_units(query_type, query_list, query_ife)
         rejected_members, ec_members, ec_id, nr_release = em.get_ec_members(query_ife)
-        corr_complete, corr_std = cs.get_correspondence(query_units, ec_members[:20])
+        corr_complete, corr_std = cs.get_correspondence(query_units, bound[:25])
         ife_list, coord_data = ui.build_coord(corr_complete)
         # Get the pairwise annotation for the instances in the EC
         pw_info, pw_sorted = ps.get_pairwise_annotation(corr_complete, query_units, ife_list)
@@ -174,6 +152,9 @@ def correspondence_geometric(method, ife, selection, core):
         corr_complete = ui.merge_list(core_complete, corr_std)
         # Get the pairwise annotation for the instances in the EC
         pw_info, pw_sorted = ps.get_pairwise_annotation(corr_std, query_units, ife_list)
+        # Get the tertiary pairwise annotation
+        pw_lr = ps.get_pairwise_tertiary(corr_complete, ife_list)
+        rp_contacts = ps.get_pairwise_rnap(corr_complete, ife_list)
         # Get the center data for calculating discrepancy
         center_data = ccs.get_center(corr_complete)
         # Calculate discrepancy using the geometric method
@@ -189,6 +170,8 @@ def correspondence_geometric(method, ife, selection, core):
         publication_year = ui.get_annotation_new(ifes_ordered)
         # Reorder the pairwise annotation based on the new ordering
         pw_info_ordered = ui.reorder_pw(ifes_ordered, pw_info)
+        pw_lr_ordered = ui.reorder_pw(ifes_ordered, pw_lr)
+        rp_contacts_ordered = ui.reorder_pw(ifes_ordered, rp_contacts)
         return render_template("correspondence_display.html", query_nts=query_units,
                                coord=coord_ordered, coord_core=None, ifes=ifes_ordered, maxDisc=max_disc, p2=percentile,
                                data=heatmap_data, trna_occupancy=trna_occupancy, functional_state=functional_state,
@@ -198,4 +181,5 @@ def correspondence_geometric(method, ife, selection, core):
                                description=description, structure_method=structure_method,
                                structure_resolution=structure_resolution, principal_investigator=principal_investigator,
                                publication_year=publication_year, pw_info=pw_info_ordered, pw_list=pw_sorted,
-                               release_id=nr_release, ec_id=ec_id)
+                               pw_tertiary=pw_lr_ordered, rp_contacts=rp_contacts_ordered, release_id=nr_release,
+                               ec_id=ec_id)
