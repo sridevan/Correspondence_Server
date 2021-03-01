c06bcbb8078fc388739a327922373999d2020148
Sri Devan <sria@bgsu.edu>
2021-02-28 20:59:47 -0500

    Added import statements for the interactions blueprint

--- corr_server/app.py
+++ corr_server/app.py
@@ -9,7 +9,7 @@ sys.path.insert(0, folder)
 app = flask.Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1/rna3dhub?' \
+    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1/rna3dhub-next?' \
                                  'unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock'
 
 db = SQLAlchemy(app)
@@ -28,10 +28,12 @@ def register_blueprints():
     import views.correspondence_views
     import views.home_views
     import views.double_correspondence_views
+    import views.interaction_views
 
     app.register_blueprint(views.correspondence_views.blueprint)
     app.register_blueprint(views.home_views.blueprint)
     app.register_blueprint(views.double_correspondence_views.blueprint)
+    app.register_blueprint(views.interaction_views.blueprint)
 
 
 if __name__ == '__main__':
