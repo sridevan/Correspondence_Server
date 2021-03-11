8ac6217a9c36c24626e085d3217c0901fafc41cf
Sri Devan <sria@bgsu.edu>
2021-03-10 20:58:50 -0500

    Updated the pairwise contacts reference dict

--- corr_server/views/ribosome_annotation_views.py
+++ corr_server/views/ribosome_annotation_views.py
@@ -20,22 +20,30 @@ reference_contacts = {
     'ssu_nts_mrna': ['5J7L|1|AA|G|926', '5J7L|1|AA|4OC|1402', '5J7L|1|AA|C|1403'],
     'ssu_nts_trna_p': ['5J7L|1|AA|G|1338', '5J7L|1|AA|A|1339', '5J7L|1|AA|C|1400'],
     'ssu_nts_trna_a': ['5J7L|1|AA|G|530', '5J7L|1|AA|A|1492', '5J7L|1|AA|A|1493'],
+    'ssu_nts_trna_e': ['5J7L|1|AA|G|693', '5J7L|1|AA|A|694'],
     'lsu_nts_trna_e': ['5J7L|1|DA|G|2112', '5J7L|1|DA|G|2421', '5J7L|1|DA|C|2422'],
     'lsu_nts_trna_p': ['5J7L|1|DA|OMG|2251', '5J7L|1|DA|G|2252', '5J7L|1|DA|G|2253'],
     'lsu_nts_trna_a': ['5J7L|1|DA|G|2553', '5J7L|1|DA|G|2583', '5J7L|1|DA|U|2585']
 }
 
 
+"""
+#TODO
+Refactor the code to get all the correspondences at one go instead of doing it individually
+for each chain
+"""
+
+
 @blueprint.route('/ribosome')
 def annotate_ribosome_chains():
     # this needs to be ssu chain
-    test_ife = '5UYP|1|A'
+    test_ife = '5UYL|1|A'
     # test_ife = '5H5U|1|h'
     # test_ife = '4V9O|1|BA'
     ribosome_components['SSU_16S'] = test_ife
     # Get all RNA chain from the query structure
     # test_ssu is a tuple of pdb and chain of test_ife ('6BU8', 'A')
-    # rna chains is a list of rna chain from test_ife ribosome structure
+    # rna chains is a list of rna chain from test_ife ribosome structure except the test ssu
     test_ssu, rna_chains = rcs.get_rna_chain(test_ife)
     # Returns a list of corresponding nts that should interact with LSU if available in the query structure from the
     # reference
@@ -54,6 +62,7 @@ def annotate_ribosome_chains():
     # structure from the reference
     ssu_nts_ptrna_corr = rcs.get_nts_corr(reference_contacts['ssu_nts_trna_p'], test_ssu)
     # Infer P-site tRNA chain
+    # name is ambiguous -- SSU/LSU?
     ribosome_components['trna_p'], rna_chains = rcs.infer_interacting_chain(ssu_nts_ptrna_corr, rna_chains)
     # Returns a list of corresponding nts that should interact with SSU A-site tRNA if available in the query
     # structure from the reference
@@ -64,7 +73,25 @@ def annotate_ribosome_chains():
     # Returns a list of corresponding nts that should interact with LSU E-site tRNA if available in the query
     # structure from the reference
     lsu_nts_etrna_corr = rcs.get_nts_corr(reference_contacts['lsu_nts_trna_e'], test_lsu)
+    # Returns a list of corresponding nts that should interact with SSU E-site tRNA if available in the query
+    # structure from the reference
+    ssu_nts_etrna_corr = rcs.get_nts_corr(reference_contacts['ssu_nts_trna_e'], test_ssu)
     # Infer E-site tRNA chain
     ribosome_components['trna_e'], rna_chains = rcs.infer_interacting_chain(lsu_nts_etrna_corr, rna_chains,
                                                                             ribosome_components, 'etrna')
-    return str(ribosome_components)
+    # Returns a list of corresponding nts that should interact with LSU E-site tRNA if available in the query
+    # structure from the reference
+    lsu_nts_atrna_corr = rcs.get_nts_corr(reference_contacts['lsu_nts_trna_a'], test_lsu)
+    # Returns a list of corresponding nts that should interact with LSU E-site tRNA if available in the query
+    # structure from the reference
+    lsu_nts_ptrna_corr = rcs.get_nts_corr(reference_contacts['lsu_nts_trna_p'], test_lsu)
+    # Infer A-site tRNA state
+    atrna_state = rcs.infer_tRNA_state(ribosome_components, 'trna_a', reference_contacts, lsu_nts_atrna_corr,
+                                       ssu_nts_ptrna_corr, lsu_nts_ptrna_corr)
+    # Infer P-site tRNA state
+    ptrna_state = rcs.infer_tRNA_state(ribosome_components, 'trna_p', reference_contacts, lsu_nts_ptrna_corr,
+                                       ssu_nts_etrna_corr, lsu_nts_etrna_corr)
+    # Infer E-site tRNA state
+    etrna_state = rcs.infer_tRNA_state(ribosome_components, 'trna_e')
+    trna_states = (atrna_state, ptrna_state, etrna_state)
+    return str(ribosome_components) + " " + str(trna_states)
